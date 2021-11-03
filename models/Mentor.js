a740baf0788c76f8c040310d4eb616ec379db1f8
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2021-11-03 18:16:10 -0500

    CRUD hecho y con cors

---
+++ models/Mentor.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const MentorSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    dni: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    created_date: {
+        type: Date,
+        default: Date.now()
+    },
+    mentorizeds: {
+        type: [mongoose.Schema.Types.ObjectId]
+    },
+    tier: {
+        type: Number
+    },
+    hv: {
+        type: {
+            education:String,
+            profile:String,
+            experience:Number
+        }
+    }
+});
+
+module.exports = mongoose.model('Mentor', MentorSchema);
